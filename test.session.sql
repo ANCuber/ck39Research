18d263a9c165a012a6a4334fe2015d0a61787409
willyistaken <95692620+willyistaken@users.noreply.github.com>
2022-05-30 15:56:07 +0800

    remove websites

--- test.session.sql
+++ test.session.sql
@@ -3,20 +3,13 @@ SHOW DATABASES
 
 
 --@block
-INSERT INTO data(latex) VALUE ("\\frac{2}{3}");
-
+INSERT INTO data(latex) VALUE ("x^3");
 
 
 --@block
 SELECT * from data
 
 --@block
-SELECT * from data WHERE done=0 ORDER BY RAND() LIMIT 1
-
---@block
-UPDATE DATA
-SET description='哈摟'
-WHERE id=1
-
---@block
-DELETE FROM data
+SELECT * from data
+ORDER BY done DESC
+LIMIT 2
