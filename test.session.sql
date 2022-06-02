ce71e60ba4402078326e508b97b4bfb22fe0898e
willyistaken <95692620+willyistaken@users.noreply.github.com>
2022-06-02 16:31:33 +0800

    save

--- test.session.sql
+++ test.session.sql
@@ -3,13 +3,32 @@ SHOW DATABASES
 
 
 --@block
-INSERT INTO data(latex) VALUE ("x^3");
+INSERT INTO data(latex) VALUE ("orz");
 
 
 --@block
 SELECT * from data WHERE done=1
 
+
 --@block
-SELECT * from data
+SELECT * from data WHERE id=1
 ORDER BY done DESC
-LIMIT 2
+
+--@block
+DELETE FROM data WHERE description IS NULL;
+
+
+
+--@block
+UPDATE DATA SET done=0 WHERE id=901;
+
+
+
+
+
+--@block
+
+SELECT COUNT(*) FROM data WHERE done=1
+--@block
+
+SELECT COUNT(*) FROM data
