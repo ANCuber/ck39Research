eadb37361b0c3460b1ddb7584a678cd92b5fbbae
= <=>
2022-07-07 15:10:17 +0800

    7/6 clean up folder and started tokenizer

--- huggingface-demo.py
+++
@@ -1,7 +0,0 @@
-from transformers import pipeline, set_seed 
-import torch
-if torch.cuda.is_available():
-    print(1);
-generator = pipeline('text-generation', model='gpt2')
-set_seed(42)
-print(generator("What you have done is indeed too", max_length=50, num_return_sequences=1)[0]['generated_text'])
