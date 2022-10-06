908af8e579ba78f5bdb9e57638690c19d062c63c
willyistaken <willyhlchan@gmail.com>
2022-10-06 16:15:17 +0800

    tools

--- data/tools/labeler.py
+++ data/tools/labeler.py
@@ -1,15 +1,18 @@
-
-
 import matplotlib.pyplot as plt
 
 filedir = input("input the location of file you want to label:")
-
+writefiledir = input("input the location of file you want to write to:")
+plt.ion()
+plt.show()
 with open(filedir,'r',encoding='utf-8') as f:
     arr = f.readlines()
-    ax = plt.subplot(111)
     for line in arr:
         line = line.strip('\n')
         print(line)
         a = line
-        ax.text(0.5,0.5,r"$%s$" %(a),fontsize=30,color="green")
-        plt.show()
+        ax = plt.subplot(111)
+        ax.text(0,0.5,r"$%s$" %(a),fontsize=30,color="green")
+        des = input("input description:");
+        ax.clear()
+        with open(writefiledir,'a',encoding='utf-8') as w:
+            w.writelines(a+","+des+"\n");
