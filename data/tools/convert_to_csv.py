7698aa7b88f044f91e57927dfd8b722ad56bd336
willyistaken <willyhlchan@gmail.com>
2022-10-07 11:23:06 +0800

    label tool and csv convertor

---
+++ data/tools/convert_to_csv.py
@@ -0,0 +1,16 @@
+import csv
+
+filedir = input("input the file you want to convert(.txt):")
+writefile = input("input the folder you want to put csv into:")
+name = input("name your csv file:")
+
+csvname = writefile+r"/"+name+".csv"
+with open(filedir,'r',encoding = 'utf-8') as f:
+    arr = f.readlines()
+    for line in arr:
+        if(line[0:3]=="---"):
+            continue
+        pairstuff = line.split("||,||")
+        with open(csvname,'w') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(pairstuff)
--- data/tools/labeler.py
+++ data/tools/labeler.py
@@ -4,15 +4,34 @@ filedir = input("input the location of file you want to label:")
 writefiledir = input("input the location of file you want to write to:")
 plt.ion()
 plt.show()
+curlinen = 0
+try:
+    with open(writefiledir,'r',encoding='utf-8') as rl:
+        arr = rl.readlines()
+        curlinen = int((arr[-1])[3:-3])
+    print("starting at line {}".format(curlinen))
+except:
+    print("create new file at:"+writefiledir)
 with open(filedir,'r',encoding='utf-8') as f:
     arr = f.readlines()
-    for line in arr:
+    for i in range(curlinen,(len(arr))):
+        line = arr[i]
         line = line.strip('\n')
         print(line)
         a = line
         ax = plt.subplot(111)
-        ax.text(0,0.5,r"$%s$" %(a),fontsize=30,color="green")
-        des = input("input description:");
         ax.clear()
+        try:
+            ax.text(0,0.5,r"$%s$" %(a),fontsize=30,color="green")
+            des = input("input description:");
+        except:
+            print("there is an error in LaTex commands")
+            continue;
+        if(des=="skip"):
+            continue
+        if(des=='exit' or des=='quit'):
+            with open(writefiledir,'a',encoding='utf-8') as bw:
+                bw.writelines("---{}--\n".format(i-1))
+            exit()
         with open(writefiledir,'a',encoding='utf-8') as w:
-            w.writelines(a+","+des+"\n");
+            w.writelines(a+"||,||"+des+"\n");
