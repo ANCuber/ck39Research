e07d632ea688c64eba2d2e4d09a2ddae08c181a5
ANCuber <ansona1131@gmail.com>
2022-11-24 15:07:32 +0800

    tool_update

--- tools/pre-processing.py
+++ tools/pre-processing.py
@@ -1,64 +1,16 @@
-'''
-* -> space and punctuation mark
+from processing_func import *
 
-common place holder:
- x,n,y,z,w,k,p,a,b,c,d
+filedir = input("input the location of file you want to pre-process:")
+writefile = input("input the folder you want to write to:")
 
-flow:
-    replace variable
-    replace punctuation
-    replace numbers
-'''
-import re
-def IsPuncutation(c):
-    for h in punctuation_list:
-        if(h==c):
-            return 1
-    return 0
+name = input("name your file:")
 
-variable_list = ["x","n","y","z","w","k","p","a","b","c","d","q","t","R","r"]
-punctuation_list = ['[', ']',' ','^','+','-','(',')','/','{','}','\\','=','<','>',':','!','_']
-punc_to_token = {"[":"[Plmb]","]":"[Prmb]"," ":"[Pspa]","^":"[Pexp]","+":"[Pplu]","-":"[Pmin]","(":"[Plsb]",")":"[Prsb]","/":"[Pdiv]","{":"[Plbb]","}":"[Prbb]","\\":"[Pbeg]","=":"[Peql]","<":"[Ples]",">":"[Pbgr]",":":"[Pcol]","!":"[Pexc]","_":"[Psub]"}
-def ReplaceVariable(s):
-    s = "]"+s+"["
-    #print(s)
-    cnt = 0;
-    for g in variable_list:
-        work = 0
-        for i in range(len(s)-len(g)-1):
-            if (((s[i]!=']') or (s[i+len(g)+1]!='['))):
-                continue;
+resultfile = writefile+r"/"+name+".txt"
 
-            if(s[i+1:i+len(g)+1]==g):
-                if(work==0):
-                    work=1
-                    cnt+=1
-                s =s[:i+1]+"%|||{}|||%".format(cnt)+s[i+len(g)+1:]
-    s = s[1:-1]
-    for i in range(1,cnt+1):
-        s = s.replace("%|||{}|||%".format(i),"[var{}]".format(i))
-    return s
+with open(filedir,'r',encoding = 'utf-8') as f:
+    sour = f.readlines()
+    for line in sour:
+        with open(resultfile, 'a') as res:
+            print(ReplaceVariable(ReplacePunctuation(line)), end = '', file = res)
 
-def ReplacePunctuation(s):
-    s = s.replace('[',"%temp%").replace("]","[Prmb]").replace("%temp%","[Plmb]")
-    for i in punc_to_token:
-        if(i!='[' and i !=']'):
-            s = s.replace(i,punc_to_token[i])
-    return s
-
-def ReplaceVariableDes(s:str):
-    cnt = 0;
-    for u in variable_list:
-        v = " "+u+" ";
-        if(len(re.findall(r"(^{}[^a-zA-Z])|([^a-zA-Z]{}[^a-zA-Z])|([^a-zA-Z]{})$".format(u,u,u),s))>0):
-            cnt+=1
-            s = re.sub(r"(^)"+u+r"([^a-zA-Z])",r"\1|||{}|||\2".format(cnt),s)
-            s = re.sub(r"([^a-zA-Z])"+u+r"([^a-zA-Z])",r"\1|||{}|||\2".format(cnt),s)
-            s = re.sub(r"([^a-zA-Z])"+u+r"($)",r"\1|||{}|||\2".format(cnt),s)
-    for i in range(1,cnt+1):
-        s = s.replace("|||{}|||".format(i),"[var{}]".format(i))
-    return s;
-s = input()
-
-print(ReplaceVariableDes(s))
-print(ReplaceVariable(ReplacePunctuation(s)))
+        
