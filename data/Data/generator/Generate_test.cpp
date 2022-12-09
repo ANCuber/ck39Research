113b384d5859d1d8b088c2334586b57e7a77e094
ANCuber <ansona1131@gmail.com>
2022-12-09 11:26:56 +0800

    gendata

--- data/Data/generator/Generate_test.cpp
+++ data/Data/generator/Generate_test.cpp
@@ -1,68 +1,101 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define cout outf
-char arr[] = {'^','+','-','*','/'}; 
-char variable_list[] = {'x','m','n','y','z','w','k','p','a','b','c','d','q','t','R','r','s'};
-int SizeOfVarList = 17;
+char arr[] = {'^','+','-','*','/','l'}; 
+char unit[] = {'q','s','c','t'};//sqrt,sin,cos,tan
+map<char,string> unitmp = {{'q',"\\sqrt{"},{'s',"\\sin{"},{'c',"\\cos{"},{'t',"\\tan{"}};
+char variable_list[] = {'x','m','n','y','z','w','k','p','a','b','c','d','q','t','R','r','s','%'};
+int SizeOfVarList = 18, bin = 6, uni = 4;
 ofstream outf;
-
+const int sz_k = 1;
+const int amount = 20000;
+const int prounit = 5;
 
 struct Node{
     int depth;
-    bool type;
+    int type;
     char ope;
     Node* lc = nullptr;
     Node* rc = nullptr;
     
     void init() {
         int rd = rand()%(depth+1);
-        type = !(rd <= 1);
+        type = !(rd <= sz_k);
         if (type) {
             if (rand()%2) ope = variable_list[rand()%SizeOfVarList];
             else ope = '0'+rand()%10;
         } else {
-            ope = arr[rand()%5];
-            if(ope=='*') ope=='$';
-            lc = new Node, rc = new Node;
-            lc->depth = rc->depth = depth+1;
-            lc->init(), rc->init();
-            if(lc->type && rc->type && rc->ope>='0' && rc->ope<='9') swap(lc,rc);
+            rd = rand()%prounit;
+            if (rd == 0) {//unit
+                type = 0;
+                ope = unit[rand()%uni];
+                lc = new Node, rc = nullptr;
+                lc->depth = depth+1;
+                lc->init();
+            } else {
+                type = 2;
+                ope = arr[rand()%bin];
+                if(ope=='*') ope = '$';
+                lc = new Node, rc = new Node;
+                lc->depth = rc->depth = depth+1;
+                lc->init(), rc->init();
+                if(lc->type && rc->type && rc->ope>='0' && rc->ope<='9') swap(lc,rc);
+            }
         }
     }
     
 };
 
 
-void dfs(Node* cur) {
+void dfs(Node* cur,bool enclose) {
     if (!cur) return;
-    if (cur->type) {
-        if(cur->ope>='0' && cur->ope<='9') cout<<cur->ope;
-        else cout<<"{"<<(cur->ope)<<"}";
-    } else {
-        if(cur->ope=='/'){
-           cout<<"{\\frac";
-           dfs(cur->lc);
-           dfs(cur->rc);
-           cout<<"}";
-        }else{
-        cout<<'{';
-        dfs(cur->lc);
-        if(cur->ope!='$') cout<<(cur->ope);
-        dfs(cur->rc);
-        cout<<'}';
+    if (cur->type == 2){
+        if (cur->ope == 'l') {
+            cout<<"\\log_{";
+            dfs(cur->lc,0);
+            cout<<"}";
+            dfs(cur->rc,1);
+        } else if(cur->ope=='/'){
+            cout<<"\\frac{";
+            dfs(cur->lc,0);
+            cout<<"}{";
+            dfs(cur->rc,0);
+            cout<<"}";
+        } else if(cur->ope=='^'){
+            cout<<"{";
+            dfs(cur->lc,0);
+            cout<<"}^{";
+            dfs(cur->rc,0);
+            cout<<"}";
+        } else {
+            if(enclose) cout<<'(';
+            dfs(cur->lc,1);
+            if(cur->ope!='$') cout<<(cur->ope);
+            dfs(cur->rc,1);
+            if(enclose) cout<<')';
         }
+    } else if (cur->type == 1) {
+        //cout<<"{"<<(cur->ope)<<"}";
+        if (cur->ope == '%') cout<<"\\pi";
+        else cout<<(cur->ope);
+    } else {
+        if (enclose) cout<<'(';
+        cout<<unitmp[cur->ope];
+        dfs(cur->lc,0);
+        cout<<"}";
+        if (enclose) cout<<')';
     }
 }
 
 int main() {
     outf.open("gendata.txt");
     srand( time(NULL) ); 
-    for(int i=0;i<100000;i++){
-    Node* root = new Node;
-    root->depth = 0;
-    root->init();
-    dfs(root);
-    cout<<"\n";
+    for(int i=0;i < amount;i++){
+        Node* root = new Node;
+        root->depth = 0;
+        root->init();
+        dfs(root,0);
+        cout<<"\n";
     }
     return 0;
 }
