5356202d3dbcee18554449f75bc47e16d65dad15
= <=>
2023-01-12 15:51:30 +0800

    bm25

--- code/bm25/bm25.py
+++ code/bm25/bm25.py
@@ -9,21 +9,29 @@ def listtoword(arr):
     s = ""
     for i in arr:
         s+=i
-    s.replace(" ",'')
+    s = s.replace(' ','')
     return s
+
+
+
 
 doc_dir = "/home/12518research/ck39Research/data/Data/dpr/1000data1/newdata.csv"
 with open(doc_dir,'r') as file:
     corpus = csv.reader(file)
     tokencorpus = []
+    latex = []
+    print(len(latex))
     for line in corpus:
         line[0]= line[0].strip(' ')
         line[1] = line[1].strip(' ')
         tokencorpus.append(tokenizer.tokenize(line[1]))
+        latex.append(line[0])
     bm25 = BM25Okapi(tokencorpus)
 
-    query = "[var1]分之[var2]的[var1]次方"
+    #for loop start here
+    query = "cos[Pspa][var1]的5次方"
     tokenized_query = tokenizer.tokenize(query)
+    print(tokenized_query)
     score = bm25.get_scores(tokenized_query)
     score = score.tolist()
     top_k = 5
@@ -36,4 +44,4 @@ with open(doc_dir,'r') as file:
     for i in a:
         res.append(score.index(i))
     for i in res:
-        print(listtoword(tokencorpus[i]))
+        print(listtoword(latex[i]),listtoword(tokencorpus[i]),end='\n\n')
