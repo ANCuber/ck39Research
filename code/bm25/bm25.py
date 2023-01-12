616b53e9e6b2d36963054c8fd2c653fa1830cd5f
= <=>
2023-01-12 15:13:41 +0800

    1/12

---
+++ code/bm25/bm25.py
@@ -0,0 +1,39 @@
+from transformers import AutoTokenizer
+
+tokenizer = AutoTokenizer.from_pretrained("/home/12518research/ck39Research/code/Pretrain/model")
+
+from rank_bm25 import BM25Okapi
+
+import csv
+def listtoword(arr):
+    s = ""
+    for i in arr:
+        s+=i
+    s.replace(" ",'')
+    return s
+
+doc_dir = "/home/12518research/ck39Research/data/Data/dpr/1000data1/newdata.csv"
+with open(doc_dir,'r') as file:
+    corpus = csv.reader(file)
+    tokencorpus = []
+    for line in corpus:
+        line[0]= line[0].strip(' ')
+        line[1] = line[1].strip(' ')
+        tokencorpus.append(tokenizer.tokenize(line[1]))
+    bm25 = BM25Okapi(tokencorpus)
+
+    query = "[var1]分之[var2]的[var1]次方"
+    tokenized_query = tokenizer.tokenize(query)
+    score = bm25.get_scores(tokenized_query)
+    score = score.tolist()
+    top_k = 5
+
+    x = []
+    x.extend(score)
+    x.sort(reverse=True)
+    a = x[:top_k]
+    res = []
+    for i in a:
+        res.append(score.index(i))
+    for i in res:
+        print(listtoword(tokencorpus[i]))
